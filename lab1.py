07168c2d1afc2fe0077479616a0d8ed0451b866d
Shamal <77284310+Shamalll@users.noreply.github.com>
2023-07-11 21:15:18 -0400

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,15 @@
+# Class: CIST 2742 Python Programming I
+# Term: Summer 2023 Instructor: Chris Bishop
+# Description: Solution to Lab #1
+# Author: (Shamal Williams)# 900511291
+# By turning in this code, I Pledge:
+# 1. That I have completed the programming assignment independently.
+# 2. I have not copied the code from a student or any source.
+# 3. I have not given my code to any student.
+n = int(input("How many cookies wants to make?: "))
+sugar = (n/48)*1.5
+butter = (n/48)
+flour = (n/48)*2.75
+print("Sugar:", sugar, "cups")
+print("Butter:", butter, "cups")
+print("Flour:", flour, "cups")
